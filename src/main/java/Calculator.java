f49aeab9e8c82abada7f0740fce0a89060c9a49b
XanderTG <ajsteim6@gmail.com>
2020-10-09 08:12:59 -0600

    Added fib method
    Signed-off-by: XanderTG <ajsteim6@gmail.com>

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,22 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        /* Declare an array to store Fibonacci numbers. */
+        int f[] = new int[n+2]; // 1 extra to handle case, n = 0
+        int i;
+
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+        f[1] = 1;
+
+        for (i = 2; i <= n; i++)
+        {
+       /* Add the previous 2 numbers in the series
+         and store it */
+            f[i] = f[i-1] + f[i-2];
+        }
+
+        return f[n];
     }
 
 
